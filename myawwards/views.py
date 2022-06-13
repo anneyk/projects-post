147464b8fa8d055777dfc1dec17477777e7faa0e
Annetkhavere <anneywan5@gmail.com>
2022-06-13 09:44:29 +0300

    add the signup function

--- myawwards/views.py
+++ myawwards/views.py
@@ -41,3 +41,17 @@ class UserViewSet(viewsets.ModelViewSet):
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+def signup(request):
+  if request.method == 'POST':
+    form = SignupForm(request.POST)
+    if form.is_valid():
+      form.save()
+      username = form.cleaned_data.get('username')
+      raw_password = form.cleaned_data.get('password1')
+      user = authenticate(username=username, password=raw_password)
+      login(request, user)
+      return redirect('index')
+    else:
+      form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
