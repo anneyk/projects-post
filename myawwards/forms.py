03832b3dfa412e5e7b627e99ff19c96104b90ee8
Annetkhavere <anneywan5@gmail.com>
2022-06-13 08:59:37 +0300

    add an updateuserform class

--- myawwards/forms.py
+++ myawwards/forms.py
@@ -15,3 +15,9 @@ class PostForm(forms.ModelForm):
   class Meta:
     model = Post
     fields = ('photo', 'title', 'url', 'description', 'technologies',)
+
+class UpdateUserForm(forms.ModelForm):
+  email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+  class Meta:
+    model = User
+    fields = ('username', 'email')
