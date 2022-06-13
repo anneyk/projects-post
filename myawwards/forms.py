c5f92fa77fdb0fc7618fe257f700898a77329d4b
Annetkhavere <anneywan5@gmail.com>
2022-06-13 08:56:16 +0300

    add signup form

--- myawwards/forms.py
+++ myawwards/forms.py
@@ -3,3 +3,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Profile, Rating
 from pyuploadcare.dj.forms import ImageField
+
+class SignupForm(UserCreationForm): 
+  email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+  class Meta:
+    model = User
+    fields = ('username', 'email', 'password1', 'password2')
